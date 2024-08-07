39c18abd178fce5f64f2b6f72ff9199771a152e1
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-07 22:12:00 +0200

    Add comments

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# Import required libraries
 from flask import Flask, render_template
 
+# Create a Flask application instance
 app = Flask(__name__)
 
+# Define the route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Check if the script is run directly, and not imported as a module
 if __name__ == '__main__':
     app.run(debug=True)
