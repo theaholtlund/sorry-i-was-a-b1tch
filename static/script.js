f5b204698c12a45acb2c43d5f6cf633b08af30e9
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-09 13:31:33 +0200

    Add comments

--- static/script.js
+++ static/script.js
@@ -1,7 +1,9 @@
+// Function to make the surprise element visible
 function showSurprise() {
   document.getElementById("surprise").style.display = "block";
 }
 
+// Function to position an element randomly on the screen, avoiding overlap with a container
 function randomPosition(element) {
   const container = document.querySelector(".outer-container");
   const containerRect = container.getBoundingClientRect();
@@ -13,19 +15,23 @@ function randomPosition(element) {
   let x, y;
 
   do {
+    // Generate random x and y coordinates within the viewport, ensuring the element stays within bounds
     x = Math.floor(Math.random() * (viewportWidth - imageSize));
     y = Math.floor(Math.random() * (viewportHeight - imageSize));
   } while (
+    // Check if the element overlaps with the container, and retry if it does
     x + imageSize > containerRect.left &&
     x < containerRect.right &&
     y + imageSize > containerRect.top &&
     y < containerRect.bottom
   );
 
+  // Position the element at the calculated coordinates
   element.style.left = x + "px";
   element.style.top = y + "px";
 }
 
+// On window load, position two images randomly on the screen
 window.onload = function () {
   const img1 = document.getElementById("sorry-man");
   const img2 = document.getElementById("sorry-woman");
