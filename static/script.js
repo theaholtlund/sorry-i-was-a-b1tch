a8416cd85e14940a6f3f944a33903381dae7f9e1
theaholtlund <117310317+theaholtlund@users.noreply.github.com>
2024-08-17 11:50:43 +0200

    Initial update to image positioning

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 // Function to make the surprise element visible
 function showSurprise() {
-  document.getElementById("surprise").style.display = "block";
+  const surpriseElement = document.getElementById("surprise");
+  surpriseElement.style.display = "block";
 
   // Update the apology message after the button is clicked
   const apologyText = document.getElementById("apology-text");
@@ -10,9 +11,22 @@ function showSurprise() {
   const button = document.querySelector("button");
   button.innerText = "SURPRISE UNLOCKED!";
   button.style.backgroundColor = "#ff1493";
+
+  // Reposition images to avoid overlap with the surprise text
+  repositionImages();
 }
 
-// Function to position an element randomly on the screen, avoid overlap with main container
+// Function to reposition images to avoid overlap with the surprise text
+function repositionImages() {
+  const img1 = document.getElementById("sorry-man");
+  const img2 = document.getElementById("sorry-woman");
+
+  // Reposition images
+  randomPosition(img1);
+  randomPosition(img2);
+}
+
+// Function to position an element randomly on the screen, avoid overlap with main container and surprise text
 function randomPosition(element) {
   const container = document.querySelector(".outer-container");
   const containerRect = container.getBoundingClientRect();
@@ -24,15 +38,23 @@ function randomPosition(element) {
   let x, y;
 
   do {
-    // Generate random coordinates within viewport to ensure element stay within bounds
+    // Generate random coordinates within viewport to ensure element stays within bounds
     x = Math.floor(Math.random() * (viewportWidth - imageSize));
     y = Math.floor(Math.random() * (viewportHeight - imageSize));
   } while (
-    // Check if element overlaps with the container and retry if it does
-    x + imageSize > containerRect.left &&
-    x < containerRect.right &&
-    y + imageSize > containerRect.top &&
-    y < containerRect.bottom
+    // Check if element overlaps with the container, surprise text or apology text and retry if it does
+    (x + imageSize > containerRect.left &&
+      x < containerRect.right &&
+      y + imageSize > containerRect.top &&
+      y < containerRect.bottom) ||
+    (x + imageSize > surpriseRect.left &&
+      x < surpriseRect.right &&
+      y + imageSize > surpriseRect.top &&
+      y < surpriseRect.bottom) ||
+    (x + imageSize > apologyRect.left &&
+      x < apologyRect.right &&
+      y + imageSize > apologyRect.top &&
+      y < apologyRect.bottom)
   );
 
   // Position the element at the calculated coordinates
